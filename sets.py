5c8f55de6d4e4adc2a1ce20ae20a363422199721
https://github.com/nurseincode <nurseincode@gmail.com>
2025-09-21 01:39:39 +1000

    sets

---
+++ sets.py
@@ -0,0 +1,7 @@
+# Set - Collection of items, unordered({1, 2} same as {2, 1}) , not indexed , mutable , no duplicates
+
+names_set = {'Jane', 'Faith', 'Yuan', 'Jim'}
+
+# print(len(name_set[1])) # no index access on sets 
+names_set.add('Tom') # sets do not maintain any insertion/removal order
+print(names_set)
